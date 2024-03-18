32f59002eb6aa5e89073f1135bfbfd092c2bf7c7
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-03-18 22:23:10 +0530

    structure of nike

---
+++ Nike/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {headerLogo} from '../assets/images'
+
+
+const Nav = () => {
+  return (
+    <header>
+        <nav>
+            <a href="/">
+                <img src={headerLogo} alt="" />
+            </a>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
